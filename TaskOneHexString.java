0b022c48665d2daf424809d912af9664d478b873
Grzegorz Klusek <qsekmordo@int.pl>
2023-01-07 13:20:37 +0100

    add solution to task one

--- TaskOneHexString.java
+++ TaskOneHexString.java
@@ -1,2 +1,14 @@
+//Napisz program, który utworzy łańcuch znaków wypełniony cyframi układu szesnastkowego
+//Write a program that creates a string filled with hexadecimal digits
+
 public class TaskOneHexString {
-}
+    public static void main(String[] args) {
+        String hexString = "";
+        for (int i = 0; i < 16; i++) {
+            hexString += Character.forDigit(i, 16);
+        }
+        hexString = hexString.toUpperCase();
+        System.out.println("the hexadecimal digits: " + hexString);
+    }
+    }
+
