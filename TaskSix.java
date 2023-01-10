92d67590023d19fbcea67322bb5eb89958dda0e5
Grzegorz Klusek <gk0x@int.pl>
2023-01-10 17:45:05 +0100

    clean up code

--- TaskSix.java
+++ TaskSix.java
@@ -1,4 +1,3 @@
-
 //[pl] Napisz program zamieniający ciąg bajtów {115,122,121,102,114} na łańcuch znaków odpowiadający tym liczbom
 //[eng] Write a program that converts the byte string {115,122,121,102,114} into a string corresponding to these numbers
 
@@ -7,7 +6,7 @@ import java.util.Arrays;
 public class TaskSix {
     public static void main(String[] args) {
 
-        byte[] bytes = {115,122,121,102,114};
+        byte[] bytes = {115, 122, 121, 102, 114};
         System.out.println(Arrays.toString(bytes));
 
         String s = new String(bytes);
